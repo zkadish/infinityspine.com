d6c3e0411c3e82bdf89a7027de503d20ea051f54
zachary kadish <zachkadish@gmail.com>
2018-12-26 23:48:11 +0200

    added the splash area

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './js';
 
+import './img/logo.png';
+import './img/back-pain-01.jpg';
+
 import './index.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const jsonImporter = require('node-sass-json-importer');
+
 const devMode = process.env.NODE_ENV !== 'production';
 const mode = process.env.NODE_ENV;
 
-// NOTE: to gain more control you can make separate 
+console.log(devMode);
+
+// NOTE: to gain more control you can make separate
 // webpack configs for the js and the css
 // TODO: add postcss - autoprefixer - Verify its working
 // TODO: add css minification
 // TODO: add eslint-loader: https://medium.com/@jontorrado/working-with-webpack-4-es6-postcss-with-preset-env-and-more-93b3d77db7b2
 // TODO: add styleLint: https://medium.com/@jontorrado/working-with-webpack-4-es6-postcss-with-preset-env-and-more-93b3d77db7b2
 
-console.log('debMode:', devMode);
-console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
+// console.log('debMode:', devMode);
+// console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
 
 module.exports = {
   mode,
   entry: {
-    infinity: './src/index.js'
+    infinity: './src/index.js',
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.(sc|c)ss$/,
@@ -44,27 +47,39 @@ module.exports = {
           // },
           {
             loader: 'css-loader',
-            options: {importLoaders: 1}
+            options: { importLoaders: 1 },
           },
           {
             loader: 'postcss-loader',
             options: {
               options: {
                 config: {
-                  path: __dirname + '/postcss.config.js',
-                }
-              }
-            }
+                  path: `${__dirname} /postcss.config.js`,
+                },
+              },
+            },
           },
           {
             loader: 'sass-loader',
             options: {
               importer: jsonImporter(),
-            }
+            },
           },
         ],
-      }
-    ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img',
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -74,10 +89,10 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
-    })
+    }),
   ],
   devServer: {
-    // contentBase: path.join(__dirname, 'public'),
+    contentBase: path.join(__dirname, 'public'),
     // quiet: true,
-  }
-}
+  },
+};
