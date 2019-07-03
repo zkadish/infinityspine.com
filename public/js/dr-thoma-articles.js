8e4af757d53f9179d2d8e196d9fc0a866ae0e5ef
zachary kadish <zachkadish@gmail.com>
2019-07-03 08:23:53 -0700

    2 articles are working are the articles page

--- public/js/dr-thoma-articles.js
+++ public/js/dr-thoma-articles.js
@@ -95,11 +95,15 @@
 
 var articleTitle = document.querySelector('.article-title h1');
 var thomaArticle = document.querySelector('.thoma-articles');
-fetch('http://infinityspine.com/wp-json/wp/v2/posts?per_page=1').then(function (response) {
+var hash = window.location.hash;
+var params = hash.split('?')[1];
+var articleNum = params.slice(params.indexOf('article') + 8);
+debugger;
+fetch('http://infinityspine.com/wp-json/wp/v2/posts?per_page=2').then(function (response) {
   return response.json();
 }).then(function (posts) {
-  thomaArticle.innerHTML = posts[0].content.rendered;
-  articleTitle.innerHTML = posts[0].title.rendered;
+  thomaArticle.innerHTML = posts[articleNum].content.rendered;
+  articleTitle.innerHTML = posts[articleNum].title.rendered;
 });
 
 /***/ })
--- src/js/dr-thoma-articles.js
+++ src/js/dr-thoma-articles.js
@@ -1,10 +1,13 @@
-
 const articleTitle = document.querySelector('.article-title h1');
 const thomaArticle = document.querySelector('.thoma-articles');
+const { hash } = window.location;
+const params = hash.split('?')[1];
+const articleNum = params.slice(params.indexOf('article') + 8);
+// debugger;
 
-fetch('http://infinityspine.com/wp-json/wp/v2/posts?per_page=1')
+fetch('http://infinityspine.com/wp-json/wp/v2/posts?per_page=2')
   .then(response => response.json())
   .then((posts) => {
-    thomaArticle.innerHTML = posts[0].content.rendered;
-    articleTitle.innerHTML = posts[0].title.rendered;
+    thomaArticle.innerHTML = posts[articleNum].content.rendered;
+    articleTitle.innerHTML = posts[articleNum].title.rendered;
   });
